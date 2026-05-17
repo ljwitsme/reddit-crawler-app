22ef8bf9116487caad8b7ee4341304aa781dd865
ljwitsme <jiawenlee5@gmail.com>
2026-05-17 11:09:26 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
+import os
 import uvicorn
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(
         "backend.api.routes:app",
         host="0.0.0.0",
-        port=8000,
-        reload=True,
-        reload_dirs=["backend", "frontend"],
+        port=port,
+        reload=False,
     )
